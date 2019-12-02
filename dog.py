5c511262eed5659e3e8eed9030ce152848ac809d
guillermo <porregu@ssfs.org>
2019-12-02 08:59:56 -0500

    work in the clasworks dog

---
+++ dog.py
@@ -0,0 +1,11 @@
+class Dog:
+
+    def __init__(self, dogs_name):
+        self.dogs_name = dogs_name
+
+    def get_name(self):
+        return self.dogs_name
+
+    def print_name(self):
+        print(self.dogs_name)
+
---
+++ test_dog.py
@@ -0,0 +1,4 @@
+import dog
+
+my_dog = dog.Dog("Gaspar")
+my_dog.print_name()
